42802a98b5c19e51c973fb63b0b51a6bd20d5d5b
Krishan Kant Agnihotri <60218702+KRISHANKANTAGNIHOTRI@users.noreply.github.com>
2021-03-15 17:44:03 +0530

    Update M.cpp

--- M.cpp
+++ M.cpp
@@ -1,4 +1,9 @@
-         
+/* There are two cases.
+
+Case 1: There have only even number. In this case answer is "-1". Cause for making the even number odd by adding another number, we need a odd number (we can make even number odd by only adding a odd number with that even number)
+
+case 2: In this case there must have a odd number. Then for every even number, we can add a odd number with the target even number. And make every even number odd by one operation. Thus number of even number = minimum operation to make all odd.        
+*/
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
