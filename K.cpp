db4f1c3e8a641cd9ab152e0247f5aae70ade3cc6
Krishan Kant Agnihotri <60218702+KRISHANKANTAGNIHOTRI@users.noreply.github.com>
2021-03-15 16:22:46 +0530

    Create K.cpp

---
+++ K.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int 
+int main()
+{
+     int n;
+    cin >> n;
+    string num = to_string(n);
+    if (n < 0)
+    {
+        if (num[num.size()-1] > num[num.size()-2])
+        {
+            num.erase(num.size()-1, 1);
+        } else {
+            num.erase(num.size()-2, 1);
+        }
+    }
+    if (num != "-0")
+    {
+        cout << num;
+    } else {
+        cout << "0";
+    }
+}
