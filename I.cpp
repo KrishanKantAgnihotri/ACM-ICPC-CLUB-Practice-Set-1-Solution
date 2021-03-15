a4cad5ca1b421fe91a19b2055c92b4e01f541112
Krishan Kant Agnihotri <60218702+KRISHANKANTAGNIHOTRI@users.noreply.github.com>
2021-03-15 16:14:19 +0530

    Create I.cpp

---
+++ I.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+int main()
+{
+int t;
+cin>>t;
+//t = 1;
+while(t--)
+{
+ ll n,m;
+ cin>>n>>m;
+ if(n%m==0)
+ cout<<"YES\n";
+ else
+ cout<<"NO\n";
+}
+}
